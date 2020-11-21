78c25cfc57759f3ac4ff06d29676b73508642a0c
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-11-21 14:06:11 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
-const routes = require('./routes')
 const flash = require('connect-flash')
 
 const app = express()
+const routes = require('./routes')
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
   next()
 })
 
